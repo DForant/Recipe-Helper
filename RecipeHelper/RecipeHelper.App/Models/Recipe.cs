964da07dead890ad59200fc3cf7b91ff6b1f5de7
DForant <dforant@gmail.com>
2024-06-27 11:25:54 -0400

    Add Recipe Model

---
+++ RecipeHelper/RecipeHelper.App/Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipeHelper.App.Models
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+        public string? ImgUrl { get; set; }
+
+    }
+}
