8cdb22860aa86f6358e9108ab03f55ef453a038c
DForant <dforant@gmail.com>
2024-06-27 11:20:30 -0400

    Implemented ASP.NET Core Web App (Razor)

--- RecipeHelperApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RecipeHelperApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
